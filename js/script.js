03666fcba364c2471287610764b0985deab62cd5
Fededada23 <dadamofederico@gmail.com>
2022-11-27 14:11:20 +0100

    fixed variable prezzo

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-let km = parseInt('numero dei km');
-let eta = parseInt('numero eta');
+let km = parseInt(prompt('numero dei km'));
+let eta = parseInt(prompt('numero eta'));
 let prezzo = km * 0.21;
 if(eta < 18){
-    price = price - ((price/100) * 20);
+    prezzo = prezzo - ((prezzo/100) * 20);
     console.log('l"eta è inferiore a 18 anni')
 }
 else if(eta > 65){
-    price = price - ((price/100) * 40);
-    console.log('l"eta è inferiore a 65 anni')
+    prezzo = prezzo - ((prezzo/100) * 40);
+    console.log('l"eta è superiore a 65 anni')
 }
 else{
     console.log('eta normale')
 }
 
-document.getElementById('prezzo').innerHTML = 'il tuo prezzo è' + " " + price.toFixed(2)
+document.getElementById('prezzo').innerHTML = 'il tuo prezzo è ' + '' + prezzo.toFixed(2)
